9a87614103aee3de8c0f6e660f2fdd3bfc2b74ef
enums <enum@enumsblog.com>
2017-07-02 17:31:02 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Pjango-WebSocket",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0),
+        .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-WebSocket.git", majorVersion: 2),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0)
     ]
